bac254802029ba28e7f50e18c407c0d4f6120dfb
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-11-29 14:30:59 -0300

    Primerio commit

---
+++ src/Averages.java
@@ -0,0 +1,20 @@
+//Given an array, find the averages of first and last subarrays of 'jump' contiguous elements in it.
+//        {5, 13, 24, 56, 7, 8, 11, 98, 57, 23, 56, 21, 33, 72, 82, 9, 10, 48, 30, 61};
+//        Example: if jump = 3;
+//        Average of 5 and 24 is 14.5, then average of 13 and 56 is 34.5, and etc.
+//        Output all the averages into a new array or arraylist.
+//        Show in the console the higher number and the smallest number of the new array.
+
+public class Averages {
+    public static void main(String[] args) {
+        int[] list = {5, 13, 24, 56, 7, 8, 11, 98, 57, 23, 56, 21, 33, 72, 82, 9, 10, 48, 30, 61};
+        int jump = 3;
+
+        for ( int i = 0; i < list.length; i++) {
+            for ( int j = list[jump-1]; j < list.length; j++) {
+                double average = (list[i] + list[j]) / 2;
+                System.out.println("A média é: " + average);
+            }
+        }
+    }
+}
---
+++ src/IdsNames.java
@@ -0,0 +1,28 @@
+/*The following two arrays have IDs and names:
+        ids = {35, 98, 23, 43, 50};
+        names = {“John”, “Peter”, “Mariah”, “Bill”, “Chris”};
+        The IDs and names are in the respective positions in each array, id number 35 is for John, 98 for Peter, 23 for Mariah, 43 for Bill and 50 for Chris.
+        The corresponding IDs and names are in the same position in the array.
+        Sort the ids array (and names) in ascending order changing the id value to a new position starting value at 1. Ex:
+        {“23 – Mariah”, “35 – John”, “43 – Bill”, “50 – Chris”, “98 - Peter”} sorted
+        {“1 – Mariah”, “2 – John”, “3 – Bill”, “4 – Chris”, “5 - Peter”} ids changed to position starting at 1
+        However, for even positions the name should be replaced by “N/A”. Expected result is an array like:
+        {“1 – Mariah”, “2 – N/A”, “3 – Bill”, “4 – N/A”, “5 - Peter”};*/
+
+public class IdsNames {
+    public static void main(String[] args) {
+
+        int[] ids = {35, 98, 23, 43, 50};
+        String[] names = {"John", "Peter", "Mariah", "Bill", "Chris"};
+
+        for(int i = 0; i < ids.length; i++) {
+            for(int j = 0; j < ids.length; j++) {
+
+                int tempId = ids[j];
+                ids[j] = ids[i];
+                ids[i] = tempId;
+
+            }
+        }
+    }
+}
