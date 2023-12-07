70b216ad2cb53a27e8dc4009a0340a64f073bc48
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-12-07 10:24:39 -0300

    Average Finalizado!

--- src/Averages.java
+++ src/Averages.java
@@ -10,26 +10,26 @@ Given an array, find the averages of first and last subarrays of 'jump' contiguo
 public class Averages {
     public static void main(String[] args) {
         int[] array = {5, 13, 24, 56, 7, 8, 11, 98, 57, 23, 56, 21, 33, 72, 82, 9, 10, 48, 30, 61};
-        int jump = 3;
 
-        double highest = Double.MIN_VALUE;
-        double smallest = Double.MAX_VALUE;
+        double maior = Double.MIN_VALUE;
+        double menor = Double.MAX_VALUE;
 
-        for (int i = 0; i <= array.length - jump; i += jump) {
-            double sum = array[i] + array[i + jump - 1];
-            double average = sum / 2.0; // Divide by 2 to find average of two elements
+        for (int i = 0; i < array.length - 2; i++) {
+            int soma = array[i] + array[i + 2];
+            double media = (double) soma / 2;
 
-            if (average > highest) {
-                highest = average;
+            if (media > maior) {
+                maior = media;
             }
-            if (average < smallest) {
-                smallest = average;
+            if (media < menor) {
+                menor = media;
             }
 
-            System.out.println("Average of subarray starting at index " + i + ": " + average);
+            System.out.println("A soma de " + i + " + " + (i + 2) + " = " + soma);
+            System.out.println("A média do index " + i + "é: " + media);
         }
 
-        System.out.println("\nHighest number in the new array: " + highest);
-        System.out.println("Smallest number in the new array: " + smallest);
+        System.out.println("\nO número mais alto da média é: " + maior);
+        System.out.println("O menor número da média é: " + menor);
     }
 }
